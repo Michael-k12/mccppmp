acf976b7823afacd779a5b75254246029efd3851
Michael-k12 <kaquilalamichaeljosh@gmail.com>
2025-11-17 23:11:11 +0800

    asd

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,28 +1,6 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | reCAPTCHA Site Key (Public)
-    |--------------------------------------------------------------------------
-    |
-    | Used to display the reCAPTCHA widget on your site.
-    |
-    */
-
-    'site_key' => env('RECAPTCHA_SITE_KEY'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | reCAPTCHA Secret Key (Private)
-    |--------------------------------------------------------------------------
-    |
-    | Used to communicate between your site and Google for verification.
-    |
-    */
-
+    'site_key'   => env('RECAPTCHA_SITE_KEY'),
     'secret_key' => env('RECAPTCHA_SECRET_KEY'),
-    
-    // Add other necessary options for your specific reCAPTCHA package here.
 ];
