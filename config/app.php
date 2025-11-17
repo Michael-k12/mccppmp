c1438b056afe59d3e08405e9d6a90a46de3b5de3
Michael-k12 <kaquilalamichaeljosh@gmail.com>
2025-11-18 07:45:33 +0800

    asd

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Procuremnet'),
+    'name' => env('APP_NAME', 'Project Procurement Management System'),
 
     /*
     |--------------------------------------------------------------------------
