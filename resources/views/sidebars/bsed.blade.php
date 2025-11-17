22b051e24cefd4bd198646278769e88bf73d2141
Michael-k12 <kaquilalamichaeljosh@gmail.com>
2025-11-17 20:35:16 +0800

    asd

--- resources/views/sidebars/bsed.blade.php
+++ resources/views/sidebars/bsed.blade.php
@@ -2,14 +2,14 @@
     Dashboard
 </flux:navlist.item>
 <flux:navlist.item icon="plus" :href="route('ppmp.create')" wire:navigate.prefetch>
-    Create 
+    Add Procurement Plan
 </flux:navlist.item>
 <flux:navlist.item icon="pencil" :href="route('ppmp.manage')" wire:navigate>
     Manage 
 </flux:navlist.item>
 <flux:navlist.item icon="arrow-right" :href="route('ppmp.view')" wire:navigate>
-    Submit 
+    Submit
 </flux:navlist.item>
-<flux:navlist.item icon="table-cells" :href="route('ppmp.bsed')" wire:navigate>
-    Anual Project Plan
+<flux:navlist.item icon="table-cells" :href="route('ppmp.bsit')" wire:navigate>
+    Procurement Records
 </flux:navlist.item>
